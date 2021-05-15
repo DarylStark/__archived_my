6cba77ff5c80a73a519a529e9e97690ce6761d8a
Daryl Stark <daryl@dstark.nl>
2021-05-15 15:17:29 +0200

    Added DOCSTRINGs to service packages

--- src/my_rest_api_v1/__init__.py
+++ src/my_rest_api_v1/__init__.py
@@ -1,3 +1,9 @@
+"""
+    The my_rest_api_v1 package is the main package that will be used for the
+    REST API v1 service of the `my-dstark-nl` application. It will contain the
+    application code that starts the REST API and run the REST API.
+"""
+# ------------------------------------------------------------------------------
 # Imports
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
