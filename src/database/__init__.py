e3b71c0466de00aaf06f48f509704c6299439770
Daryl Stark <daryl@dstark.nl>
2021-05-15 16:26:58 +0200

    Created a Database package

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,9 +1,11 @@
 """
-    The database pacakge will be a wrapper for the SQLalchemy package and will
+    The database package will be a wrapper for the SQLalchemy package and will
     contain classes and methods to interact with databases.
 """
 # ------------------------------------------------------------------------------
 # Imports
+from database.database import Database
+from database.database_session import DatabaseSession
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
     raise NotImplementedError('This file can not be ran as script')
