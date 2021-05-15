6432b39a9b1210ed45602e74810619afa81ed251
Daryl Stark <daryl@dstark.nl>
2021-05-15 15:18:47 +0200

    Started with the 'database' package

---
+++ src/database/__init__.py
@@ -0,0 +1,10 @@
+"""
+    The database pacakge will be a wrapper for the SQLalchemy package and will
+    contain classes and methods to interact with databases.
+"""
+# ------------------------------------------------------------------------------
+# Imports
+# ------------------------------------------------------------------------------
+if __name__ == '__main__':
+    raise NotImplementedError('This file can not be ran as script')
+# ------------------------------------------------------------------------------
