1a2d44af960ba4ddc421d8fab1296884d3334003
Daryl Stark <daryl@dstark.nl>
2021-05-19 21:11:34 +0200

    Convert the RESTAPIEndpointURL class to a dataclass

--- src/rest_api_generator/rest_api_endpoint_url.py
+++ src/rest_api_generator/rest_api_endpoint_url.py
@@ -5,24 +5,15 @@
 # ---------------------------------------------------------------------
 # Imports
 from rest_api_generator.rest_api_endpoint import RESTAPIEndpoint
+from dataclasses import dataclass
 # ---------------------------------------------------------------------
 
 
+@dataclass
 class RESTAPIEndpointURL:
     """ Class used to create objects that contain a URL as string and
         a RESTAPIEndpoint object """
 
-    def __init__(self, url: str, endpoint: RESTAPIEndpoint):
-        """ The initiator sets the URL and the endpoint
-
-            Parameters
-            ----------
-            url : str
-                The url that belongs to this RESTAPIEndpoint
-
-            endpoint : RESTAPIEndpoint
-                The RESTAPIEndpoint object for this URL
-        """
-        self.url = url
-        self.endpoint = endpoint
+    url: str
+    endpoint: RESTAPIEndpoint
 # ---------------------------------------------------------------------
