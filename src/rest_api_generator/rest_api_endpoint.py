d0608c99bcafe5cb124336ef8e3311719b5f4906
Daryl Stark <daryl@dstark.nl>
2021-05-20 20:03:39 +0200

    Convert the RESTAPIEndpoint to a dataclass

--- src/rest_api_generator/rest_api_endpoint.py
+++ src/rest_api_generator/rest_api_endpoint.py
@@ -4,47 +4,36 @@
 """
 # ---------------------------------------------------------------------
 from typing import List, Optional
+from dataclasses import dataclass
 # ---------------------------------------------------------------------
 
 
+@dataclass
 class RESTAPIEndpoint:
-    """ Class that represent a API endpoint """
+    """ Class that represent a API endpoint
 
-    def __init__(self,
-                 url_suffix: str,
-                 func,
-                 name: Optional[str] = None,
-                 description: Optional[str] = None,
-                 http_methods: Optional[List[str]] = None):
-        """ Inititator sets default values for the endpoint.
+        Members
+        -------
+        url_suffix: str
+            The URL suffix for the endpoint.
 
-            Parameters
-            ----------
-            url_suffix : str
-                The URL suffix for the endpoint.
+        func:
+            The method / function to run for this endpoint
 
-            func : 
-                The method/function to run for this endpoint
+        name: str(default=None)
+            The name for the API endpoint. Is used in help pages
 
-            name : str (default=None)
-                The name for the API endpoint. Is used in help pages
+        description: str(default=None)
+            Description for the API endpoint. Is used in help pages
 
-            description : str (default=None)
-                Description for the API endpoint. Is used in help pages
+        http_methods: List[str](default=None)
+            HTTP methods that this API endpoint supports
+    """
 
-            http_methods : List[str] (default=None)
-                HTTP methods that this API endpoint supports
-
-            Returns
-            -------
-            method
-                The decorator
-        """
-
-        # Set the values
-        self.url_suffix: str = url_suffix
-        self.func = func
-        self.name: Optional[str] = name
-        self.description: Optional[str] = description
-        self.http_methods: Optional[List[str]] = http_methods
+    # Set the values
+    url_suffix: str
+    func: str
+    name: Optional[str]
+    description: Optional[str]
+    http_methods: Optional[List[str]]
 # ---------------------------------------------------------------------
