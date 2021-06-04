4b2f5b5f063dcfa34dab30ca60167a742da5c6ae
Daryl Stark <daryl@dstark.nl>
2021-06-04 15:56:16 +0200

    Changed the REST API so that the authorization method is in a different module

--- src/my_rest_api_v1/__init__.py
+++ src/my_rest_api_v1/__init__.py
@@ -8,12 +8,11 @@
 # Imports
 from typing import List, Optional, Union
 from flask import Flask
-from rest_api_generator import RESTAPIGenerator, RESTAPIAuthorization, \
-    BasicAuthorization, BearerAuthorzation
+from rest_api_generator import RESTAPIGenerator
 from rich.logging import RichHandler
 from my_rest_api_v1.api import api_group_api
 import logging
-from my_database.api_authorization import get_token_information
+from my_rest_api_v1.authorization import authorization
 # ---------------------------------------------------------------------
 # Configure logging
 logging.basicConfig(
@@ -32,8 +31,9 @@ my_rest_api_v1 = RESTAPIGenerator(
     bp_import_name=__name__,
     bp_url_prefix='/api/v1/')
 
-# Turn on authorization
+# Turn on authorization and set the authorization method
 my_rest_api_v1.use_authorization = True
+my_rest_api_v1.authorization_function = authorization
 
 # Make sure errors get reported as JSON error resposne instead of
 # normal Flask error pages
@@ -43,64 +43,6 @@ my_rest_api_v1.abort_on_error = False
 my_rest_api_v1.accept_method('POST')
 my_rest_api_v1.accept_method('PATCH')
 my_rest_api_v1.accept_method('DELETE')
-
-
-# Create a method for authorization
-@my_rest_api_v1.register_authorization_method
-def auth(auth: Optional[Union[BasicAuthorization, BearerAuthorzation]],
-         scopes: Optional[List[str]]) -> RESTAPIAuthorization:
-    """
-        Method that does the authentication for the REST API.
-
-        Parameters
-        ----------
-        auth : Optional[Union[BasicAuthorization, BearerAuthorzation]]
-            The authentication header from the Flask Request. Can be
-            None, a BasicAuthorization object or a BearerAuthorization
-            object, depending on the given authorization.
-
-        scopes : Optional[List[str]]
-            The scopes that are defined in the endpoint that are
-            required for this endpoint.
-
-        Returns
-        -------
-        RESTAPIAuthorization:
-            A object containing the authorization information.
-    """
-
-    # Create a authorization object
-    auth_object: RESTAPIAuthorization = RESTAPIAuthorization()
-
-    # Check the type of authorization we received. We only accept the
-    # 'Bearer' kind, since that is being used in OAuth world.
-    if type(auth) is not BearerAuthorzation:
-        return auth_object
-
-    # Get the token object
-    token_object = get_token_information(auth.token)
-
-    # If we didn't get a object, the token is wrong
-    if token_object is None:
-        auth_object.authorized = False
-        return auth_object
-
-    # Get the assosicated scopes
-    token_scopes = [
-        token_scope.scope.full_scope_name
-        for token_scope in token_object.token_scopes
-    ]
-
-    # Check if any of the given scopes is in the 'token scopes'
-    for scope in scopes:
-        if scope in token_scopes:
-            auth_object.authorized = True
-            auth_object.data = token_object
-            break
-
-    # Return authorization object
-    return auth_object
-
 
 # Register the created groups
 my_rest_api_v1.register_group(group=api_group_api)
