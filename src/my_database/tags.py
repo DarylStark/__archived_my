7cd00e162bf76b13fd2abb281b80d95e61932dff
Daryl Stark <daryl@dstark.nl>
2021-08-23 20:34:01 +0200

    Created the method to add tags to the `my_database`-package

--- src/my_database/tags.py
+++ src/my_database/tags.py
@@ -3,118 +3,82 @@
     database.
 """
 from typing import List, Optional
+import sqlalchemy
 from database import DatabaseSession
-from my_database.generic import delete_object
-from my_database_model import User, Tag
-from sqlalchemy.orm.query import Query
+from my_database_model import Tag, User
 from my_database import logger
-from my_database.exceptions import FilterNotValidError, IntegrityError, NotFoundError
+from my_database.exceptions import (FilterNotValidError, IntegrityError,
+                                    NotFoundError)
+from my_database.exceptions import (FilterNotValidError,
+                                    IntegrityError, PermissionDeniedError,
+                                    NotFoundError)
+from rest_api_generator.exceptions import ServerError
 
 
-def get_tags(
-    req_user: Optional[User],
-    flt_id: Optional[int] = None
-) -> Optional[List[Tag]]:
-    """ Method that retrieves all, or a subset of, the tags in the
-        database.
+def create_tag(req_user: User, **kwargs: dict) -> Optional[Tag]:
+    """" Method to create a tag
 
         Parameters
         ----------
         req_user : User
             The user who is requesting this. Should be used to verify
-            what results the user gets. If this is set to 'None',
-            we bypass this check.
+            what the user is allowed to do.
 
-        flt_id : Optional[int]
-            Filter on a specific tag ID.
-
-        Returns
-        -------
-        List[Tag]
-            A list with the resulting tags.
-
-        None
-            No tags are found.
-    """
-
-    # Empty data list
-    data_list: Optional[Query] = None
-    rv: Optional[List[Tag]] = None
-
-    # Get the token
-    with DatabaseSession(commit_on_end=False, expire_on_commit=False) \
-            as session:
-        # First, we get all tags
-        data_list = session.query(Tag)
-
-        # If a user is specified, we use that user to filter the tags
-        if req_user:
-            data_list = data_list.filter(Tag.user_id == req_user.id)
-
-        # Now, we can apply the correct filters
-        try:
-            if flt_id:
-                flt_id = int(flt_id)
-                data_list = data_list.filter(Tag.id == flt_id)
-        except (ValueError, TypeError):
-            logger.error(
-                f'Tag id should be of type {int}, not {type(flt_id)}.')
-            raise FilterNotValidError(
-                f'Tag id should be of type {int}, not {type(flt_id)}.')
-
-        # Get the data
-        if data_list is not None:
-            rv = data_list.all()
-
-    # Return the data
-    return rv
-
-
-def delete_tag(
-    req_user: Optional[User],
-    tag_id: int
-) -> bool:
-    """ Method to delete a tag.
-
-        Parameters
-        ----------
-        req_user : Optional[User]
-            The user who is requesting this. Should be used to verify
-            what the user is allowed to do. If this is set to 'None',
-            we bypass this check.
-
-        tag_id : int
-            The ID for the tag to delete.
+        **kwargs : dict
+            A dict containing the fields for the tag.
 
         Returns
         -------
-        bool
-            True on success.
+        Tag
+            The created tag object.
+
     """
 
-    # Get the resource object
-    resource: Optional[List[Tag]] = get_tags(req_user, flt_id=tag_id)
+    # Check if we have all fields
+    needed_fields = [
+        'title'
+    ]
+    for field in needed_fields:
+        if field not in kwargs.keys():
+            raise TypeError(
+                f'Missing required argument "{field}"')
 
-    # Check if we got a resource. If we didn't, we rise an error
-    # indicating that the resource wasn't found. This can be either
-    # because it didn't exist, or because the user has no permissions
-    # to it.
-    if resource is None or len(resource) == 0:
-        raise NotFoundError(f'Tag with ID {tag_id} is not found.')
+    logger.debug('create_tag: all needed fields are given')
 
-    # Appearently we have resources. Because the result is a list, we
-    # we can assume the first one in the list is the one we are
-    # interested in.
-    resource = resource[0]
+    # Set the optional fields
+    optional_fields = list()
 
-    # Delete the resource
+    # Check if no other fields are given
+    possible_fields = needed_fields + optional_fields
+    for field in kwargs.keys():
+        if field not in possible_fields:
+            raise TypeError(
+                f'Unexpected field "{field}"')
+
     try:
-        # Delete the resource itself
-        delete_object(resource)
-    except IntegrityError:
+        with DatabaseSession(
+            commit_on_end=True,
+            expire_on_commit=True
+        ) as session:
+            # Create the resource
+            new_resource = Tag(
+                user=req_user,
+                title=kwargs['title']
+            )
+
+            logger.debug('create_tag: adding tag')
+
+            # Add the resource
+            session.add(new_resource)
+
+            # Return the created resource
+            return new_resource
+    except sqlalchemy.exc.IntegrityError as e:
+        logger.error(f'create_tag: IntegrityError: {str(e)}')
         # Add a custom text to the exception
-        raise IntegrityError(
-            'Tag couldn\'t be deleted because it still has resources ' +
-            'connected to it')
-    else:
-        return True
+        raise IntegrityError('Tag already exists')
+    except Exception as e:
+        logger.error(f'create_tag: Exception: {str(e)}')
+        raise ServerError(e)
+
+    return None
