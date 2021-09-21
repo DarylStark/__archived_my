5e279580cfe4839d6327f42c50fa09881fc3d7de
Daryl Stark <daryl@dstark.nl>
2021-09-21 21:26:16 +0200

    Created a Flask static directory for static files (CSS, images and JS)

--- src/my_ganymade/static.py
+++ src/my_ganymade/static.py
@@ -1,15 +1,14 @@
 """ Module that creates the Flask Blueprint for the static frontend
-    files of the My Ganymade service. """
+    files of the My Ganymade service. This Blueprint is only here to
+    serve static files and does not contain any Flask-routed functions.
+"""
 
 from flask.blueprints import Blueprint
 
 blueprint_static = Blueprint(
     name='my_ganymade_static',
     import_name=__name__,
-    url_prefix='/'
+    url_prefix='/static/',
+    static_folder='my_ganymage/static/',
+    static_url_path='/'
 )
-
-
-@blueprint_static.route('/')
-def index():
-    return 'Static pages'
