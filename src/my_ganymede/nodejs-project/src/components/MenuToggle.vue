a7388492e626c77a73b61d6f1395362ea37c1453
Daryl Stark <daryl@dstark.nl>
2021-10-09 17:07:16 +0200

    Created a central store for statemanagement and a component to hide the navigation

---
+++ src/my_ganymede/nodejs-project/src/components/MenuToggle.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="menutoggle">
+    <i class="fas fa-bars"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuToggle',
+};
+</script>
--- src/my_ganymede/nodejs-project/src/layout/Header.vue
+++ src/my_ganymede/nodejs-project/src/layout/Header.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="header">Header</div>
+  <div class="header">
+    <MenuToggle></MenuToggle>
+  </div>
 </template>
 
 <script>
+import MenuToggle from '../components/MenuToggle.vue';
+
 export default {
   name: 'Header',
+  components: { MenuToggle },
 };
 </script>
