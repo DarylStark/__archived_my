9b329b6eb719d43ed84d12a994836654dfa63322
Daryl Stark <daryl@dstark.nl>
2021-10-12 19:57:19 +0200

    Added a component for a spacer and added it to the header

--- src/my_ganymede/nodejs-project/src/layout/Header.vue
+++ src/my_ganymede/nodejs-project/src/layout/Header.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="header">
     <MenuToggle></MenuToggle>
+    <Spacer></Spacer>
   </div>
 </template>
 
 <script>
 import MenuToggle from '../components/MenuToggle.vue';
+import Spacer from '../components/Spacer.vue';
 
 export default {
   name: 'Header',
-  components: { MenuToggle },
+  components: { MenuToggle, Spacer },
 };
 </script>
