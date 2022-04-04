6c8242509c1b3579b3c1c4c95712b309955c097c
Daryl Stark <daryl@dstark.nl>
2022-04-04 16:17:29 +0200

    Started with the navigationitems

---
+++ src/my_ganymede/nodejs-project/src/components/NavigationItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="item">
+        <div class="icon">
+            <i class="fas fa-user-circle"></i>
+        </div>
+        <div class="text"><slot></slot></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NavigationItem',
+};
+</script>
--- src/my_ganymede/nodejs-project/src/layout/Navigation.vue
+++ src/my_ganymede/nodejs-project/src/layout/Navigation.vue
@@ -1,16 +1,31 @@
 <template>
-  <div v-bind:class="['navigation', { hide: !visible }]">
-    Navigation: {{ visible }}
-  </div>
+    <div v-bind:class="['navigation', { hide: !visible }]">
+        <NavigationSection title="First section" hide_title>
+            <NavigationItem>Item 1</NavigationItem>
+            <NavigationItem>Item 2</NavigationItem>
+        </NavigationSection>
+        <NavigationSection title="Second section">
+            <NavigationItem>Item 3</NavigationItem>
+            <NavigationItem>Item 4</NavigationItem>
+            <NavigationItem>Item 5</NavigationItem>
+        </NavigationSection>
+    </div>
 </template>
 
 <script>
+import NavigationItem from '../components/NavigationItem.vue';
+import NavigationSection from '../components/NavigationSection.vue';
+
 export default {
-  name: 'Navigation',
-  computed: {
-    visible() {
-      return this.$store.state.ui.navigation_visible;
+    name: 'Navigation',
+    computed: {
+        visible() {
+            return this.$store.state.ui.navigation_visible;
+        },
     },
-  },
+    components: {
+        NavigationItem,
+        NavigationSection,
+    },
 };
 </script>
