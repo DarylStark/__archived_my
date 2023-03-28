a9e10dd55dfac6807ebd8d00dcd69f41be77b654
Daryl Stark <daryl@dstark.nl>
2023-03-28 21:08:15 +0200

    Added a favicon. Fixes #96

--- src/my_web_ui/root.py
+++ src/my_web_ui/root.py
@@ -1,9 +1,10 @@
 """ Module that creates the Flask Blueprint for the root page of
     the application """
 
+import os
 from typing import Optional
 
-from flask import redirect
+from flask import redirect, send_from_directory
 from flask.blueprints import Blueprint
 
 # Create the blueprint for the UI pages
@@ -12,6 +13,22 @@ blueprint_root = Blueprint(
     import_name=__name__,
     url_prefix='/'
 )
+
+
+@blueprint_root.route(
+    '/favicon.ico',
+    methods=['GET']
+)
+def favicon() -> str:
+    """ The favicon for the application """
+
+    return send_from_directory(
+        os.path.join(
+            os.path.dirname(__file__),
+            'static',
+            'favicon'),
+        'nerd-face.ico',
+        mimetype='image/vnd.microsoft.icon')
 
 
 @blueprint_root.route(
